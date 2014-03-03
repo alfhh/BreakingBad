51e6673f94dc4d454344c13af4a60708a0f04eb7
Alfredo Hinojosa <alfhh4@gmail.com>
2014-03-03 14:18:42 -0600

    Proyecto Nuevo

---
+++ src/breakingbad/BreakingBad.java
@@ -0,0 +1,18 @@
+/**
+ * @author Alfredo Hinojosa Huerta A01036053
+ * @author Luis Juan Sanchez A01183634
+ * @version 1.0 3/3/2014
+ */
+
+package breakingbad;
+
+public class BreakingBad {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
