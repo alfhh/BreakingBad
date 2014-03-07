b24b269360ee2b8fd49c01c656140bd845b55b71
Luis Juan Sanchez <luisjuansp@gmail.com>
2014-03-07 02:56:14 -0600

    Movimiento de la pelota

--- src/breakingbad/BreakingBad.java
+++ src/breakingbad/BreakingBad.java
@@ -28,10 +28,15 @@ public class BreakingBad extends JFrame implements Runnable, MouseListener, KeyL
 
     private Animacion animPaleta; // Animacion de la Paleta (Jugador)
     private Paleta paleta; // Objeto de la Paleta
-    private int pMovx; // Movimiento de la Paleta
+    private Animacion animPelota; // Animacion de la Pelota
+    private Pelota pelota; // Objeto de Pelota
+    private int peMovy; // Movimiento de la Pelota en el eje y
+    private int peMovx; // Movimiento de la Pelota en el eje x
+    private int paMov; // Movimiento de la Paleta
     private Image dbImage; // Imagen
     private Graphics dbg; // Objeto Grafico
     private boolean pausa; // Flag de pausa
+    private boolean inicio; // Flag de inicio
 
     /**
      * El metodo constructor de la clase BreakingBad
@@ -42,18 +47,28 @@ public class BreakingBad extends JFrame implements Runnable, MouseListener, KeyL
         setSize(1280, 720);
         setTitle("Breaking Bad: The Game");
         pausa = false;
-        
+        inicio = false;
+        peMovx = paMov = 0;
+        peMovy = 5;
+
         // Animacion de la Paleta
         Image b0 = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("images/fedora.png"));
         animPaleta = new Animacion();
         animPaleta.sumaCuadro(b0, 100);
-        
-        
+
+        // Animacion de la Pelota
+        b0 = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("images/pelota.png"));
+        animPelota = new Animacion();
+        animPelota.sumaCuadro(b0, 100);
+
         paleta = new Paleta(0, 0, animPaleta);
         paleta.setPosX(this.getWidth() / 2 - paleta.getAncho() / 2);
-        paleta.setPosY(this.getHeight() - paleta.getAlto());
-        
-        
+        paleta.setPosY(this.getHeight() - paleta.getAlto() - 10);
+
+        pelota = new Pelota(0, 0, animPelota);
+        pelota.setPosX(paleta.getPosX() + paleta.getAncho() / 2 - pelota.getAncho() / 2);
+        pelota.setPosY(paleta.getPosY() - pelota.getAlto());
+
         addMouseListener(this);
         addKeyListener(this);
         Thread th = new Thread(this);
@@ -62,9 +77,9 @@ public class BreakingBad extends JFrame implements Runnable, MouseListener, KeyL
 
     public void run() {
         while (true) {
-            if (!pausa){
-             checaColision();
-             actualiza();   
+            if (!pausa && inicio) {
+                checaColision();
+                actualiza();
             }
             repaint();
             try {
@@ -76,15 +91,34 @@ public class BreakingBad extends JFrame implements Runnable, MouseListener, KeyL
     }
 
     public void actualiza() {
-        if (paleta.getPosX() + pMovx > 0
-                && paleta.getPosX() + pMovx + paleta.getAncho() < this.getWidth()) {
-            paleta.setPosX(paleta.getPosX() + pMovx);
-            pMovx = 0;
+        if (paleta.getPosX() + paMov > 0
+                && paleta.getPosX() + paMov + paleta.getAncho() < this.getWidth()) {
+            paleta.setPosX(paleta.getPosX() + paMov);
+            paMov = 0;
         }
+        pelota.setPosX(pelota.getPosX()+peMovx);
+        pelota.setPosY(pelota.getPosY()+peMovy);
     }
 
     public void checaColision() {
+        if (pelota.getPosX() < 0 || pelota.getPosX() + pelota.getAncho() > this.getWidth()) {
+            peMovx = -peMovx;
+        }
+        if (pelota.getPosY() < 0) {
+            peMovy = -peMovy;
+        }
+        if (pelota.getPosY() + pelota.getAlto() > this.getHeight()) {
 
+        }
+        if (paleta.getPerimetro().intersects(pelota.getPerimetro())) {
+            int ca = (pelota.getPosX() + pelota.getAncho() / 2)
+                    - (paleta.getPosX() + paleta.getAncho() / 2);
+            int co = (pelota.getPosY())
+                    - (paleta.getPosY());
+            int h = (int) Math.sqrt(Math.pow(ca, 2) + Math.pow(co, 2));
+            peMovx = 20 * ca / h;
+            peMovy = 20 * co / h;
+        }
     }
 
     /**
@@ -115,6 +149,9 @@ public class BreakingBad extends JFrame implements Runnable, MouseListener, KeyL
 
         if (paleta.getAnimacion() != null) {
             g.drawImage(paleta.animacion.getImagen(), paleta.getPosX(), paleta.getPosY(), this);
+        }
+        if (pelota.getAnimacion() != null) {
+            g.drawImage(pelota.animacion.getImagen(), pelota.getPosX(), pelota.getPosY(), this);
         }
     }
 
@@ -163,16 +200,22 @@ public class BreakingBad extends JFrame implements Runnable, MouseListener, KeyL
      * @param e
      */
     public void keyPressed(KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-            pMovx = -30;
-        }
+        if (inicio) {
+            if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+                paMov = -30;
+            }
 
-        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-            pMovx = 30;
+            if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+                paMov = 30;
+            }
+
+            if (e.getKeyCode() == KeyEvent.VK_P) {
+                pausa = !pausa;
+                paMov = 0;
+            }
         }
-        
-        if (e.getKeyCode() == KeyEvent.VK_P) {
-            pausa = !pausa;
+        if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+            inicio = true;
         }
     }
 
---
+++ src/breakingbad/Pelota.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package breakingbad;
+
+import javax.swing.ImageIcon;
+import java.awt.Image;
+
+public class Pelota extends Base {
+ 
+    public Pelota (int posX, int posY, Animacion animacion) {
+        super(posX, posY, animacion);
+    }
+}
